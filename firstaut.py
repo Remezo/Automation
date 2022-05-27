9e03956e774bfc88b913fbcc27f465fec4261336
miremezo <miremezo@davidson.edu>
2022-05-27 09:48:13 -0400

    Update firstaut.py

--- firstaut.py
+++ firstaut.py
@@ -1,1 +1,13 @@
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+print("now =", now)
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
+
+
 print("Hello world")
